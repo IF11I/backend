68efbba9148059087b14412196d61e2f57e8c810
Florian Weinzierl <Florian@wzma.de>
2018-07-26 11:15:50 +0200

    Fixed dummy files

--- dummyFiles/componentsDummy.php
+++ dummyFiles/componentsDummy.php
@@ -10,17 +10,19 @@ $components[] = [
     'dateWarrantyEnd' => "2010-12-31",
     'notes' => "Erster PC",
     'manufacturer' => "Lenovo",
-    'componentTypeId' => "1"
+    'componentTypeId' => "1",
+    'name' => ""
 ];
 foreach($components as $component) {
     $componentsEntity = new \Entities\componentEntity();
     $componentsEntity->setRaumId(utf8_decode($component['roomId']));
-    $componentsEntity->setLieferantenId(utf8_decode($component['supplierID']));
+    $componentsEntity->setLieferantenId(utf8_decode($component['supplierId']));
     $componentsEntity->setEinkaufsdatum(utf8_decode($component['datePurchased']));
     $componentsEntity->setGewaehrleistungsende(utf8_decode($component['dateWarrantyEnd']));
     $componentsEntity->setNotiz(utf8_decode($component['notes']));
     $componentsEntity->setHersteller(utf8_decode($component['manufacturer']));
     $componentsEntity->setKomponentenartId(utf8_decode($component['componentTypeId']));
+    $componentsEntity->setBezeichnung(utf8_decode($component['name']));
     $entityManager->persist($componentsEntity);
 }
 
